51714f9dea5ece0add0bd94761efd81143ca0047
Jesse Gallagher <jesse@secondfoundation.org>
2016-04-22 17:31:15 -0400

    Modified root build.sh to terminate on individual-step failure

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #/bin/sh
 set -e
-compiler/vm/build.sh
-mvn -f compiler/pom.xml clean install
-mvn -f dist/pom.xml clean install
-mvn -f plugins/templates/pom.xml clean install
-mvn -f plugins/idea/pom.xml clean install -Pdeployment
-mvn -f plugins/eclipse/pom.xml clean install
+compiler/vm/build.sh &&
+mvn -f compiler/pom.xml clean install &&
+mvn -f dist/pom.xml clean install && 
+mvn -f plugins/templates/pom.xml clean install &&
+mvn -f plugins/idea/pom.xml clean install -Pdeployment &&
+mvn -f plugins/eclipse/pom.xml clean install &&
 ./plugins/gradle/gradlew -b plugins/gradle/build.gradle clean assemble install
