ad6804e2d6da1fc9692ca88b40cf351bc91929ba
xoppa <contact@xoppa.com>
2016-05-13 12:15:59 +0200

    Back to 2.1.0-SNAPSHOT. Maven checks out the repository during release, so no VM binaries are available. Added compiler/vm/build.sh execution to compiler/rt/pom.xml as a profile. Activate via -Pbuild-vm-binaries

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
 #/bin/sh
 set -e
-compiler/vm/build.sh --build=release
-mvn clean install
+mvn clean install -Pbuild-natives
 mvn -f plugins/idea/pom.xml clean install
 mvn -f plugins/eclipse/pom.xml clean install
 ./plugins/gradle/gradlew -b plugins/gradle/build.gradle clean assemble install
