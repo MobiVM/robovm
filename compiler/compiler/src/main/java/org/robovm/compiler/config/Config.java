337aa1af611f26852b6c7853ac5e6c53212605b5
florianf <florian.falkner@general-solutions.at>
2016-06-19 20:31:09 +0200

    remove wip files before pull.

--- compiler/compiler/src/main/java/org/robovm/compiler/config/Config.java
+++ compiler/compiler/src/main/java/org/robovm/compiler/config/Config.java
@@ -67,7 +67,6 @@ import org.robovm.compiler.plugin.PluginArgument;
 import org.robovm.compiler.plugin.TargetPlugin;
 import org.robovm.compiler.plugin.annotation.AnnotationImplPlugin;
 import org.robovm.compiler.plugin.debug.DebugInformationPlugin;
-import org.robovm.compiler.plugin.debug.DebugInstrumentPlugin;
 import org.robovm.compiler.plugin.lambda.LambdaPlugin;
 import org.robovm.compiler.plugin.objc.InterfaceBuilderClassesPlugin;
 import org.robovm.compiler.plugin.objc.ObjCBlockPlugin;
