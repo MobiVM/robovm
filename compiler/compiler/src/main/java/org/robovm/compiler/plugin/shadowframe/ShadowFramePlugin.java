15ce2e951e57df23393b7f49f549ac5449f8992b
florianf <florian.falkner@general-solutions.at>
2016-06-27 22:57:03 +0200

    Removed unused allocation instruction

--- compiler/compiler/src/main/java/org/robovm/compiler/plugin/shadowframe/ShadowFramePlugin.java
+++ compiler/compiler/src/main/java/org/robovm/compiler/plugin/shadowframe/ShadowFramePlugin.java
@@ -8,7 +8,6 @@ import org.robovm.compiler.ModuleBuilder;
 import org.robovm.compiler.Types;
 import org.robovm.compiler.clazz.Clazz;
 import org.robovm.compiler.config.Config;
-import org.robovm.compiler.llvm.Alloca;
 import org.robovm.compiler.llvm.BasicBlock;
 import org.robovm.compiler.llvm.Call;
 import org.robovm.compiler.llvm.Function;
@@ -41,13 +40,6 @@ public class ShadowFramePlugin extends AbstractCompilerPlugin {
         if (method.isNative() || method.isAbstract() || !method.hasActiveBody()) {
             return;
         }
-        
-        // insert generation of shadow frame and wiring it up to previously set
-        // shadow frame in the entry basic block
-        BasicBlock entryBlock = function.getBasicBlocks().get(0);
-        Variable shadowVariable = function.newVariable(SHADOW_FRAME_VAR_NAME, Types.SHADOW_FRAME_PTR);
-        Alloca shadowAlloca = new Alloca(shadowVariable, Types.SHADOW_FRAME);
-        entryBlock.getInstructions().add(0, shadowAlloca);
         
         // get functionsAddress for shadowframe
         String functionSignature = function.getSignature();
