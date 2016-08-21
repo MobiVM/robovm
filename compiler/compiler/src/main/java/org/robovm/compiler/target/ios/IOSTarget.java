2b026a055f54cac12b44208899bee2cf8ab2836f
Xoppa <contact@xoppa.com>
2016-08-21 11:32:20 +0200

    Updated compiler pom so Eclipse doesn't complain about missing m2e connectors

--- compiler/compiler/src/main/java/org/robovm/compiler/target/ios/IOSTarget.java
+++ compiler/compiler/src/main/java/org/robovm/compiler/target/ios/IOSTarget.java
@@ -27,12 +27,10 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.Date;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.NoSuchElementException;
-import java.util.Set;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.FilenameUtils;
