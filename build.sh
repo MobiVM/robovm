e824698a7d43b1bcad0e9f8c0765a1426cc52271
florianf <florian.falkner@general-solutions.at>
2016-12-14 19:18:31 +0100

    Disable gradle & intellij plugins for the moment

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #/bin/sh
 set -e
 mvn clean install
-mvn -f plugins/idea/pom.xml clean install
+#mvn -f plugins/idea/pom.xml clean install
 mvn -f plugins/eclipse/pom.xml clean install
-./plugins/gradle/gradlew -b plugins/gradle/build.gradle clean assemble install
+#./plugins/gradle/gradlew -b plugins/gradle/build.gradle clean assemble install
