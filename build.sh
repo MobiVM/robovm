f9c5b3efc31b28867528e9f65a7fc8a0e942923f
Justin Shapcott <support@mobidevelop.com>
2016-12-19 08:22:33 -0700

    Update build for maven plugin changes

--- build.sh
+++ build.sh
@@ -4,6 +4,3 @@ mvn clean install
 mvn -f plugins/idea/pom.xml clean install
 mvn -f plugins/eclipse/pom.xml clean install
 ./plugins/gradle/gradlew -b plugins/gradle/build.gradle clean assemble install
-mvn -f plugins/maven/maven-resolver/pom.xml clean install
-mvn -f plugins/maven/junit/pom.xml clean install
-mvn -f plugins/maven/robovm-maven-plugin/pom.xml clean install
