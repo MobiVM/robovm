7c60e2e5758412ed529c797d6bf14797117d89e8
dkimitsa <demyan.kimitsa@gmail.com>
2017-02-02 17:17:47 +0200

    CocoaTouch.externalaccessory: ios10, added @Method annotation to constructors, re-reneration after fixing bro-gen

--- compiler/cocoatouch/src/main/java/org/robovm/apple/externalaccessory/EAAccessoryManager.java
+++ compiler/cocoatouch/src/main/java/org/robovm/apple/externalaccessory/EAAccessoryManager.java
@@ -86,6 +86,7 @@ import org.robovm.apple.uikit.*;
     /*<constants>*//*</constants>*/
     /*<constructors>*/
     public EAAccessoryManager() {}
+    protected EAAccessoryManager(Handle h, long handle) { super(h, handle); }
     protected EAAccessoryManager(SkipInit skipInit) { super(skipInit); }
     /*</constructors>*/
     /*<properties>*/
