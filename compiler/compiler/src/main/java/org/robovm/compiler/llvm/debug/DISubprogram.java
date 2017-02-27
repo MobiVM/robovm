0b1e55e00772a49dc12141ee718f91383ed1226d
florianf <florian.falkner@general-solutions.at>
2017-02-27 22:13:27 +0100

    Some further LLVM DI work

---
+++ compiler/compiler/src/main/java/org/robovm/compiler/llvm/debug/DISubprogram.java
@@ -0,0 +1,34 @@
+package org.robovm.compiler.llvm.debug;
+
+import org.robovm.compiler.llvm.debug.map.DIMap;
+import org.robovm.compiler.llvm.debug.map.DIMapValueBoolean;
+import org.robovm.compiler.llvm.debug.map.DIMapValueReference;
+
+public class DISubprogram extends DIMap {
+	public static final String DI_NAME = "DISubprogram";
+	
+	public DISubprogram() {
+		distinct = true;
+		
+		map.put("isLocal", new DIMapValueBoolean(false));
+		map.put("isDefinition", new DIMapValueBoolean(true));
+		map.put("isOptimized", new DIMapValueBoolean(false));
+	}
+	
+	public void setName(String name) {
+		put("name", name);
+	}
+	
+	public void setScope(DIMapValueReference scope) {
+		map.put("scope", scope);
+	}
+	
+	public void setFile(DIMapValueReference file) {
+		map.put("file", file);
+	}
+	
+	@Override
+	public String getDIName() {
+		return DI_NAME;
+	}
+}
--- compiler/compiler/src/main/java/org/robovm/compiler/plugin/debug/DebugInformationPlugin.java
+++ compiler/compiler/src/main/java/org/robovm/compiler/plugin/debug/DebugInformationPlugin.java
@@ -3,20 +3,25 @@ package org.robovm.compiler.plugin.debug;
 import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.List;
 
 import org.robovm.compiler.ModuleBuilder;
 import org.robovm.compiler.clazz.Clazz;
 import org.robovm.compiler.config.Config;
 import org.robovm.compiler.config.Config.Builder;
+import org.robovm.compiler.llvm.BasicBlock;
+import org.robovm.compiler.llvm.Function;
 import org.robovm.compiler.llvm.FunctionDeclaration;
 import org.robovm.compiler.llvm.FunctionRef;
 import org.robovm.compiler.llvm.FunctionType;
+import org.robovm.compiler.llvm.Instruction;
 import org.robovm.compiler.llvm.IntegerConstant;
 import org.robovm.compiler.llvm.MetadataNode;
 import org.robovm.compiler.llvm.MetadataString;
 import org.robovm.compiler.llvm.NamedMetadata;
 import org.robovm.compiler.llvm.Type;
 import org.robovm.compiler.llvm.UnnamedMetadata;
+import org.robovm.compiler.llvm.Value;
 import org.robovm.compiler.llvm.debug.DICompileUnit;
 import org.robovm.compiler.llvm.debug.DIFile;
 import org.robovm.compiler.llvm.debug.map.DIMapValueReference;
@@ -24,11 +29,10 @@ import org.robovm.compiler.log.Logger;
 import org.robovm.compiler.plugin.AbstractCompilerPlugin;
 import org.robovm.compiler.plugin.PluginArgument;
 import org.robovm.compiler.plugin.PluginArguments;
-import org.robovm.compiler.llvm.Type;
-import org.robovm.compiler.llvm.UnnamedMetadata;
-import org.robovm.compiler.llvm.UnnamedMetadataRef;
-import org.robovm.compiler.llvm.Value;
 
+import soot.SootMethod;
+import soot.Unit;
+import soot.tagkit.LineNumberTag;
 import soot.tagkit.SourceFileTag;
 
 public class DebugInformationPlugin extends AbstractCompilerPlugin {
@@ -37,6 +41,8 @@ public class DebugInformationPlugin extends AbstractCompilerPlugin {
     
     private static FunctionRef LLVM_DBG_DECLARE_FUN = new FunctionRef("llvm.dbg.declare", new FunctionType(Type.VOID, new Type[] { Type.METADATA, Type.METADATA, Type.METADATA }));
     private static FunctionDeclaration LLVM_DBG_DECLARE_DECLARATION = new FunctionDeclaration(LLVM_DBG_DECLARE_FUN);
+	private UnnamedMetadata diFileMeta;
+	private UnnamedMetadata diCuMeta;
     
     public DebugInformationPlugin() {
 	}
@@ -66,15 +72,56 @@ public class DebugInformationPlugin extends AbstractCompilerPlugin {
     	DIFile diFile = new DIFile(getSourceFile(config, clazz));
     	DICompileUnit diCu = new DICompileUnit();
     	
-    	UnnamedMetadata diCuMeta = moduleBuilder.newUnnamedMetadata(diCu);
-    	diCu.setFile(new DIMapValueReference(moduleBuilder.newUnnamedMetadata(diFile)));
+    	diCuMeta = moduleBuilder.newUnnamedMetadata(diCu);
+    	diFileMeta = moduleBuilder.newUnnamedMetadata(diFile);
+    	diCu.setFile(new DIMapValueReference(diFileMeta));
     	
     	UnnamedMetadata dwarfVersion = moduleBuilder.newUnnamedMetadata(new MetadataNode(new Value[]{ new IntegerConstant(2), new MetadataString("Dwarf Version"), new IntegerConstant(2) }));
     	UnnamedMetadata debugInfoVersion = moduleBuilder.newUnnamedMetadata(new MetadataNode(new Value[]{ new IntegerConstant(2), new MetadataString("Debug Info Version"), new IntegerConstant(2) }));
+    	UnnamedMetadata picLevel = moduleBuilder.newUnnamedMetadata(new MetadataNode(new Value[]{ new IntegerConstant(1), new MetadataString("PIC Level"), new IntegerConstant(2) }));
     	
     	moduleBuilder.addNamedMetadata(new NamedMetadata("llvm.db.cu", diCuMeta));
-    	moduleBuilder.addNamedMetadata(new NamedMetadata("llvm.module.flags", new UnnamedMetadata[] { dwarfVersion, debugInfoVersion }));
+    	moduleBuilder.addNamedMetadata(new NamedMetadata("llvm.module.flags", new UnnamedMetadata[] { dwarfVersion, debugInfoVersion, picLevel }));
     	moduleBuilder.addFunctionDeclaration(LLVM_DBG_DECLARE_DECLARATION);
+    }
+    
+    @Override
+    public void afterMethod(Config config, Clazz clazz, SootMethod method, ModuleBuilder moduleBuilder, Function function) throws IOException {
+    	super.afterMethod(config, clazz, method, moduleBuilder, function);
+    	
+
+        // don't try to generate shadow frames for native or abstract methods
+        // or methods that don't have any instructions in them
+        if (method.isNative() || method.isAbstract() || !method.hasActiveBody()) {
+            return;
+        }
+        
+        BasicBlock entryBlock = function.getBasicBlocks().get(0);
+        
+        //Method has only a return null statement
+        if (entryBlock.getInstructions().size() == 1) {
+        	return;
+        }
+        
+        int methodLineNumber = 0;
+        for (BasicBlock bb : function.getBasicBlocks()) {
+            for (int i = 0; i < bb.getInstructions().size(); i++) {
+                Instruction instruction = bb.getInstructions().get(i);
+                List<Object> units = instruction.getAttachments();
+                int currentLineNumber = -1;
+                for (Object object : units) {
+                    if (object instanceof Unit) {
+                        Unit unit = (Unit) object;
+                        
+                        LineNumberTag tag = (LineNumberTag) unit.getTag("LineNumberTag");
+                        if (tag != null) {
+                        	currentLineNumber = tag.getLineNumber();
+                        	methodLineNumber = Math.min(methodLineNumber, currentLineNumber);
+                        }
+                    }
+                }
+            }
+        }
     }
     
     private File getSourceFile(Config config, Clazz clazz) {
