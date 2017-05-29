85593c22032256423cfaaeb4c0b5f825e1d655e2
dkimitsa <demyan.kimitsa@gmail.com>
2017-05-29 12:32:42 +0300

    network extension -- updated to ios 10.3

---
+++ compiler/cocoatouch/src/main/java/org/robovm/apple/networkextension/NEAppProxyFlow.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright (C) 2013-2015 RoboVM AB
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.robovm.apple.networkextension;
+
+/*<imports>*/
+import java.io.*;
+import java.nio.*;
+import java.util.*;
+import org.robovm.objc.*;
+import org.robovm.objc.annotation.*;
+import org.robovm.objc.block.*;
+import org.robovm.rt.*;
+import org.robovm.rt.annotation.*;
+import org.robovm.rt.bro.*;
+import org.robovm.rt.bro.annotation.*;
+import org.robovm.rt.bro.ptr.*;
+import org.robovm.apple.foundation.*;
+import org.robovm.apple.security.*;
+/*</imports>*/
+
+/*<javadoc>*/
+/**
+ * @since Available in iOS 9.0 and later.
+ */
+/*</javadoc>*/
+/*<annotations>*/@Library("NetworkExtension") @NativeClass/*</annotations>*/
+/*<visibility>*/public/*</visibility>*/ class /*<name>*/NEAppProxyFlow/*</name>*/ 
+    extends /*<extends>*/NSObject/*</extends>*/ 
+    /*<implements>*//*</implements>*/ {
+
+    /*<ptr>*/public static class NEAppProxyFlowPtr extends Ptr<NEAppProxyFlow, NEAppProxyFlowPtr> {}/*</ptr>*/
+    /*<bind>*/static { ObjCRuntime.bind(NEAppProxyFlow.class); }/*</bind>*/
+    /*<constants>*//*</constants>*/
+    /*<constructors>*/
+    public NEAppProxyFlow() {}
+    protected NEAppProxyFlow(Handle h, long handle) { super(h, handle); }
+    protected NEAppProxyFlow(SkipInit skipInit) { super(skipInit); }
+    /*</constructors>*/
+    /*<properties>*/
+    /**
+     * @since Available in iOS 9.0 and later.
+     */
+    @Property(selector = "metaData")
+    public native NEFlowMetaData getMetaData();
+    /*</properties>*/
+    /*<members>*//*</members>*/
+    /*<methods>*/
+    /**
+     * @since Available in iOS 9.0 and later.
+     */
+    @Method(selector = "openWithLocalEndpoint:completionHandler:")
+    public native void open(NWHostEndpoint localEndpoint, @Block VoidBlock1<NSError> completionHandler);
+    /**
+     * @since Available in iOS 9.0 and later.
+     */
+    @Method(selector = "closeReadWithError:")
+    public native void closeReadWithError(NSError error);
+    /**
+     * @since Available in iOS 9.0 and later.
+     */
+    @Method(selector = "closeWriteWithError:")
+    public native void closeWriteWithError(NSError error);
+    /*</methods>*/
+}
---
+++ compiler/cocoatouch/src/main/java/org/robovm/apple/networkextension/NEIPv6Settings.java
@@ -0,0 +1,97 @@
+/*
+ * Copyright (C) 2013-2015 RoboVM AB
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.robovm.apple.networkextension;
+
+/*<imports>*/
+import java.io.*;
+import java.nio.*;
+import java.util.*;
+import org.robovm.objc.*;
+import org.robovm.objc.annotation.*;
+import org.robovm.objc.block.*;
+import org.robovm.rt.*;
+import org.robovm.rt.annotation.*;
+import org.robovm.rt.bro.*;
+import org.robovm.rt.bro.annotation.*;
+import org.robovm.rt.bro.ptr.*;
+import org.robovm.apple.foundation.*;
+import org.robovm.apple.security.*;
+/*</imports>*/
+
+/*<javadoc>*/
+/**
+ * @since Available in iOS 9.0 and later.
+ */
+/*</javadoc>*/
+/*<annotations>*/@Library("NetworkExtension") @NativeClass/*</annotations>*/
+/*<visibility>*/public/*</visibility>*/ class /*<name>*/NEIPv6Settings/*</name>*/ 
+    extends /*<extends>*/NSObject/*</extends>*/ 
+    /*<implements>*//*</implements>*/ {
+
+    /*<ptr>*/public static class NEIPv6SettingsPtr extends Ptr<NEIPv6Settings, NEIPv6SettingsPtr> {}/*</ptr>*/
+    /*<bind>*/static { ObjCRuntime.bind(NEIPv6Settings.class); }/*</bind>*/
+    /*<constants>*//*</constants>*/
+    /*<constructors>*/
+    public NEIPv6Settings() {}
+    protected NEIPv6Settings(Handle h, long handle) { super(h, handle); }
+    protected NEIPv6Settings(SkipInit skipInit) { super(skipInit); }
+    /**
+     * @since Available in iOS 9.0 and later.
+     */
+    @Method(selector = "initWithAddresses:networkPrefixLengths:")
+    public NEIPv6Settings(NSArray<?> addresses, NSArray<NSNumber> networkPrefixLengths) { super((SkipInit) null); initObject(init(addresses, networkPrefixLengths)); }
+    /*</constructors>*/
+    /*<properties>*/
+    /**
+     * @since Available in iOS 9.0 and later.
+     */
+    @Property(selector = "addresses")
+    public native NSArray<?> getAddresses();
+    /**
+     * @since Available in iOS 9.0 and later.
+     */
+    @Property(selector = "networkPrefixLengths")
+    public native NSArray<NSNumber> getNetworkPrefixLengths();
+    /**
+     * @since Available in iOS 9.0 and later.
+     */
+    @Property(selector = "includedRoutes")
+    public native NSArray<NEIPv6Route> getIncludedRoutes();
+    /**
+     * @since Available in iOS 9.0 and later.
+     */
+    @Property(selector = "setIncludedRoutes:")
+    public native void setIncludedRoutes(NSArray<NEIPv6Route> v);
+    /**
+     * @since Available in iOS 9.0 and later.
+     */
+    @Property(selector = "excludedRoutes")
+    public native NSArray<NEIPv6Route> getExcludedRoutes();
+    /**
+     * @since Available in iOS 9.0 and later.
+     */
+    @Property(selector = "setExcludedRoutes:")
+    public native void setExcludedRoutes(NSArray<NEIPv6Route> v);
+    /*</properties>*/
+    /*<members>*//*</members>*/
+    /*<methods>*/
+    /**
+     * @since Available in iOS 9.0 and later.
+     */
+    @Method(selector = "initWithAddresses:networkPrefixLengths:")
+    protected native @Pointer long init(NSArray<?> addresses, NSArray<NSNumber> networkPrefixLengths);
+    /*</methods>*/
+}
