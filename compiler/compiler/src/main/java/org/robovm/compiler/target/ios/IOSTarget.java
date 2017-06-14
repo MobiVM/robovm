c5a006f1c9f644b26f9174c15719430257199a1c
dkimitsa <demyan.kimitsa@gmail.com>
2017-06-14 13:13:01 +0300

    retroperspective: this change is missing but present in master, check https://github.com/MobiDevelop/robovm/commit/3d0d71ebf76d53e801eff864bdda420ffcc58114

--- compiler/compiler/src/main/java/org/robovm/compiler/target/ios/IOSTarget.java
+++ compiler/compiler/src/main/java/org/robovm/compiler/target/ios/IOSTarget.java
@@ -438,7 +438,7 @@ public class IOSTarget extends AbstractTarget {
             args.add(entitlementsPList);
         }
         if (preserveMetadata) {
-            args.add("--preserve-metadata=identifier,entitlements,resource-rules");
+            args.add("--preserve-metadata=identifier,entitlements");
         }
         if (verbose) {
             args.add("--verbose");
