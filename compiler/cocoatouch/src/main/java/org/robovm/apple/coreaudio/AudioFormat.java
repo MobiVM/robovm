ebbc857845721558fe63eb9c0f0663a327a5e21c
Tomski <tomwojciechowski@asidik.com>
2017-07-26 16:16:02 +0100

    Remove UInt marshaler on AudioFormat, fixes #170

--- compiler/cocoatouch/src/main/java/org/robovm/apple/coreaudio/AudioFormat.java
+++ compiler/cocoatouch/src/main/java/org/robovm/apple/coreaudio/AudioFormat.java
@@ -34,7 +34,7 @@ import org.robovm.apple.corefoundation.*;
 /*<javadoc>*/
 
 /*</javadoc>*/
-/*<annotations>*/@Marshaler(ValuedEnum.AsMachineSizedUIntMarshaler.class)/*</annotations>*/
+/*<annotations>*//*</annotations>*/
 public enum /*<name>*/AudioFormat/*</name>*/ implements ValuedEnum {
     /*<values>*/
     LinearPCM(1819304813L),
