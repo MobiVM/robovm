5b9dbbb84ee83ce7db7808fba8b5977d918984a5
Demyan Kimitsa <demyan.kimitsa@gmail.com>
2017-10-08 23:26:57 +0300

    ios11 bindings: MetalKit processed. TODO: NSError subclasses were manually reverted, this has to be reworked with nserror enums

--- compiler/cocoatouch/src/main/java/org/robovm/apple/metalkit/MTKTypeConverter.java
+++ compiler/cocoatouch/src/main/java/org/robovm/apple/metalkit/MTKTypeConverter.java
@@ -50,28 +50,52 @@ import org.robovm.apple.coreanimation.*;
     /*<properties>*//*</properties>*/
     /*<members>*//*</members>*/
     /*<methods>*/
+    /**
+     * @since Available in iOS 9.0 and later.
+     */
     @Bridge(symbol="MTKModelIOVertexDescriptorFromMetal", optional=true)
     public static native MDLVertexDescriptor convertVertexDescriptor(MTLVertexDescriptor metalDescriptor);
+    /**
+     * @since Available in iOS 10.0 and later.
+     */
     public static MDLVertexDescriptor convertVertexDescriptorEx(MTLVertexDescriptor metalDescriptor) throws NSErrorException {
        NSError.NSErrorPtr ptr = new NSError.NSErrorPtr();
        MDLVertexDescriptor result = convertVertexDescriptorEx(metalDescriptor, ptr);
        if (ptr.get() != null) { throw new NSErrorException(ptr.get()); }
        return result;
     }
+    /**
+     * @since Available in iOS 10.0 and later.
+     */
     @Bridge(symbol="MTKModelIOVertexDescriptorFromMetalWithError", optional=true)
     private static native MDLVertexDescriptor convertVertexDescriptorEx(MTLVertexDescriptor metalDescriptor, NSError.NSErrorPtr error);
+    /**
+     * @since Available in iOS 9.0 and later.
+     */
     @Bridge(symbol="MTKMetalVertexDescriptorFromModelIO", optional=true)
     public static native MTLVertexDescriptor convertVertexDescriptor(MDLVertexDescriptor modelIODescriptor);
+    /**
+     * @since Available in iOS 10.0 and later.
+     */
     public static MTLVertexDescriptor convertVertexDescriptorEx(MDLVertexDescriptor modelIODescriptor) throws NSErrorException {
        NSError.NSErrorPtr ptr = new NSError.NSErrorPtr();
        MTLVertexDescriptor result = convertVertexDescriptorEx(modelIODescriptor, ptr);
        if (ptr.get() != null) { throw new NSErrorException(ptr.get()); }
        return result;
     }
+    /**
+     * @since Available in iOS 10.0 and later.
+     */
     @Bridge(symbol="MTKMetalVertexDescriptorFromModelIOWithError", optional=true)
     private static native MTLVertexDescriptor convertVertexDescriptorEx(MDLVertexDescriptor modelIODescriptor, NSError.NSErrorPtr error);
+    /**
+     * @since Available in iOS 9.0 and later.
+     */
     @Bridge(symbol="MTKModelIOVertexFormatFromMetal", optional=true)
     public static native MDLVertexFormat convertVertexFormat(MTLVertexFormat vertexFormat);
+    /**
+     * @since Available in iOS 9.0 and later.
+     */
     @Bridge(symbol="MTKMetalVertexFormatFromModelIO", optional=true)
     public static native MTLVertexFormat convertVertexFormat(MDLVertexFormat vertexFormat);
     /*</methods>*/
