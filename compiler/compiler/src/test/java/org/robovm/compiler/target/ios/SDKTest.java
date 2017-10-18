a6b39f26a00bce9c4d0c0b883581acd3f58eb688
Kees van Dieren <keesvandieren@squins.com>
2017-10-18 08:27:01 +0200

    Run test only when Xcode is installed

--- compiler/compiler/src/test/java/org/robovm/compiler/target/ios/SDKTest.java
+++ compiler/compiler/src/test/java/org/robovm/compiler/target/ios/SDKTest.java
@@ -2,6 +2,7 @@ package org.robovm.compiler.target.ios;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.robovm.compiler.util.ToolchainUtil.isXcodeInstalled;
 
 import java.io.File;
 
@@ -31,8 +32,9 @@ public class SDKTest {
 
     @Test
     public void listSimulatorSdksShouldNotCrash() {
-
-        // No real asserts, as response depends on building machine. At least it should not throw an exception.
-        assertNotNull(SDK.listSimulatorSDKs());
+        if (isXcodeInstalled()) {
+            // No real asserts, as response depends on building machine. At least it should not throw an exception.
+            assertNotNull(SDK.listSimulatorSDKs());
+        }
     }
 }
