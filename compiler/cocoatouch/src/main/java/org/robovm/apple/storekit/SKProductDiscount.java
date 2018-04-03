b843f47bbfc788fd6428a12699f4429d8576508a
Demyan Kimitsa <demyan.kimitsa@gmail.com>
2018-04-03 18:11:08 +0300

    ios11.3: StoreKit processed

--- compiler/cocoatouch/src/main/java/org/robovm/apple/storekit/SKProductDiscount.java
+++ compiler/cocoatouch/src/main/java/org/robovm/apple/storekit/SKProductDiscount.java
@@ -1,12 +1,12 @@
 /*
  * Copyright (C) 2013-2015 RoboVM AB
- *
+ * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * 
  *      http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -33,63 +33,33 @@ import org.robovm.apple.uikit.*;
 
 /*<javadoc>*/
 /**
- * @since Available in iOS 3.0 and later.
+ * @since Available in iOS 11.2 and later.
  */
 /*</javadoc>*/
 /*<annotations>*/@Library("StoreKit") @NativeClass/*</annotations>*/
-/*<visibility>*/public/*</visibility>*/ class /*<name>*/SKProduct/*</name>*/ 
+/*<visibility>*/public/*</visibility>*/ class /*<name>*/SKProductDiscount/*</name>*/ 
     extends /*<extends>*/NSObject/*</extends>*/ 
     /*<implements>*//*</implements>*/ {
 
-    /*<ptr>*/public static class SKProductPtr extends Ptr<SKProduct, SKProductPtr> {}/*</ptr>*/
-    /*<bind>*/static { ObjCRuntime.bind(SKProduct.class); }/*</bind>*/
+    /*<ptr>*/public static class SKProductDiscountPtr extends Ptr<SKProductDiscount, SKProductDiscountPtr> {}/*</ptr>*/
+    /*<bind>*/static { ObjCRuntime.bind(SKProductDiscount.class); }/*</bind>*/
     /*<constants>*//*</constants>*/
     /*<constructors>*/
-    public SKProduct() {}
-    protected SKProduct(Handle h, long handle) { super(h, handle); }
-    protected SKProduct(SkipInit skipInit) { super(skipInit); }
+    public SKProductDiscount() {}
+    protected SKProductDiscount(Handle h, long handle) { super(h, handle); }
+    protected SKProductDiscount(SkipInit skipInit) { super(skipInit); }
     /*</constructors>*/
     /*<properties>*/
     /**
-     * @since Available in iOS 3.0 and later.
-     */
-    @Property(selector = "localizedDescription")
-    public native String getLocalizedDescription();
-    /**
-     * @since Available in iOS 3.0 and later.
-     */
-    @Property(selector = "localizedTitle")
-    public native String getLocalizedTitle();
-    /**
-     * @since Available in iOS 3.0 and later.
+     * @since Available in iOS 11.2 and later.
      */
     @Property(selector = "price")
     public native NSDecimalNumber getPrice();
     /**
-     * @since Available in iOS 3.0 and later.
+     * @since Available in iOS 11.2 and later.
      */
     @Property(selector = "priceLocale")
     public native NSLocale getPriceLocale();
-    /**
-     * @since Available in iOS 3.0 and later.
-     */
-    @Property(selector = "productIdentifier")
-    public native String getProductIdentifier();
-    /**
-     * @since Available in iOS 6.0 and later.
-     */
-    @Property(selector = "isDownloadable")
-    public native boolean isDownloadable();
-    /**
-     * @since Available in iOS 6.0 and later.
-     */
-    @Property(selector = "downloadContentLengths")
-    public native NSArray<NSNumber> getDownloadContentLengths();
-    /**
-     * @since Available in iOS 6.0 and later.
-     */
-    @Property(selector = "downloadContentVersion")
-    public native String getDownloadContentVersion();
     /**
      * @since Available in iOS 11.2 and later.
      */
@@ -98,8 +68,13 @@ import org.robovm.apple.uikit.*;
     /**
      * @since Available in iOS 11.2 and later.
      */
-    @Property(selector = "introductoryPrice")
-    public native SKProductDiscount getIntroductoryPrice();
+    @Property(selector = "numberOfPeriods")
+    public native @MachineSizedUInt long getNumberOfPeriods();
+    /**
+     * @since Available in iOS 11.2 and later.
+     */
+    @Property(selector = "paymentMode")
+    public native SKProductDiscountPaymentMode getPaymentMode();
     /*</properties>*/
     /*<members>*//*</members>*/
     /*<methods>*/
