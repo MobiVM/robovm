91a3b95fd365cd54c0991444b4007c598e933b1d
Dr. Daniel Thommes <daniel.thommes@nevernull.io>
2018-11-01 22:19:24 +0100

    Added copyright information and corrected copyright in CleanBuildFoldersAction

--- plugins/idea/src/main/java/org/robovm/idea/actions/CleanBuildFoldersAction.java
+++ plugins/idea/src/main/java/org/robovm/idea/actions/CleanBuildFoldersAction.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 Justin Shapcott.
+ * Copyright (C) 2018 Daniel Thommes, NeverNull GmbH, <daniel.thommes@nevernull.io>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
--- plugins/idea/src/main/java/org/robovm/idea/compilation/RoboVmCompileTask.java
+++ plugins/idea/src/main/java/org/robovm/idea/compilation/RoboVmCompileTask.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2015 RoboVM AB
+ * Copyright (C) 2018 Daniel Thommes, NeverNull GmbH, <daniel.thommes@nevernull.io>
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
