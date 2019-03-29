d3f5b5bcfdfacac389e1ca5ef3aa979121cf0d0b
Demyan Kimitsa <demyan.kimitsa@gmail.com>
2019-03-29 10:29:20 +0200

    iOS12.2: UIKit updated

--- compiler/cocoatouch/src/main/java/org/robovm/apple/uikit/UIGuidedAccess.java
+++ compiler/cocoatouch/src/main/java/org/robovm/apple/uikit/UIGuidedAccess.java
@@ -1,12 +1,12 @@
 /*
  * Copyright (C) 2013-2015 RoboVM AB
- *
+ * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * 
  *      http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -41,22 +41,23 @@ import org.robovm.apple.intents.*;
 
 /*<javadoc>*/
 /*</javadoc>*/
-/*<annotations>*//*</annotations>*/
-/*<visibility>*/public/*</visibility>*/ class /*<name>*/UILayoutPriority/*</name>*/ 
+/*<annotations>*/@Library("UIKit")/*</annotations>*/
+/*<visibility>*/public/*</visibility>*/ class /*<name>*/UIGuidedAccess/*</name>*/ 
     extends /*<extends>*/CocoaUtility/*</extends>*/ 
     /*<implements>*//*</implements>*/ {
 
     /*<ptr>*/
     /*</ptr>*/
-    /*<bind>*/static { Bro.bind(UILayoutPriority.class); }/*</bind>*/
-    /*<constants>*/
-    public static final int Required = 1000;
-    public static final int DefaultHigh = 750;
-    public static final int DefaultLow = 250;
-    public static final int FittingSizeLevel = 50;
-    /*</constants>*/
+    /*<bind>*/static { Bro.bind(UIGuidedAccess.class); }/*</bind>*/
+    /*<constants>*//*</constants>*/
     /*<constructors>*//*</constructors>*/
     /*<properties>*//*</properties>*/
     /*<members>*//*</members>*/
-    /*<methods>*//*</methods>*/
+    /*<methods>*/
+    /**
+     * @since Available in iOS 12.2 and later.
+     */
+    @Bridge(symbol="UIGuidedAccessConfigureAccessibilityFeatures", optional=true)
+    public static native void configureAccessibilityFeatures(UIGuidedAccessAccessibilityFeature features, boolean enabled, @Block VoidBlock2<Boolean, NSError> completion);
+    /*</methods>*/
 }
---
+++ compiler/cocoatouch/src/main/java/org/robovm/apple/uikit/UIGuidedAccessAccessibilityFeature.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright (C) 2013-2015 RoboVM AB
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.robovm.apple.uikit;
+
+/*<imports>*/
+import java.io.*;
+import java.nio.*;
+import java.util.*;
+import org.robovm.objc.*;
+import org.robovm.objc.annotation.*;
+import org.robovm.objc.block.*;
+import org.robovm.rt.*;
+import org.robovm.rt.annotation.*;
+import org.robovm.rt.bro.*;
+import org.robovm.rt.bro.annotation.*;
+import org.robovm.rt.bro.ptr.*;
+import org.robovm.apple.foundation.*;
+import org.robovm.apple.coreanimation.*;
+import org.robovm.apple.coregraphics.*;
+import org.robovm.apple.coredata.*;
+import org.robovm.apple.coreimage.*;
+import org.robovm.apple.coretext.*;
+import org.robovm.apple.corelocation.*;
+import org.robovm.apple.cloudkit.*;
+import org.robovm.apple.fileprovider.*;
+import org.robovm.apple.intents.*;
+/*</imports>*/
+
+/*<javadoc>*/
+/**
+ * @since Available in iOS 12.2 and later.
+ */
+/*</javadoc>*/
+/*<annotations>*/@Marshaler(Bits.AsMachineSizedIntMarshaler.class)/*</annotations>*/
+public final class /*<name>*/UIGuidedAccessAccessibilityFeature/*</name>*/ extends Bits</*<name>*/UIGuidedAccessAccessibilityFeature/*</name>*/> {
+    /*<values>*/
+    public static final UIGuidedAccessAccessibilityFeature None = new UIGuidedAccessAccessibilityFeature(0L);
+    public static final UIGuidedAccessAccessibilityFeature VoiceOver = new UIGuidedAccessAccessibilityFeature(1L);
+    public static final UIGuidedAccessAccessibilityFeature Zoom = new UIGuidedAccessAccessibilityFeature(2L);
+    public static final UIGuidedAccessAccessibilityFeature AssistiveTouch = new UIGuidedAccessAccessibilityFeature(4L);
+    public static final UIGuidedAccessAccessibilityFeature InvertColors = new UIGuidedAccessAccessibilityFeature(8L);
+    public static final UIGuidedAccessAccessibilityFeature GrayscaleDisplay = new UIGuidedAccessAccessibilityFeature(16L);
+    /*</values>*/
+
+    /*<bind>*/
+    /*</bind>*/
+    /*<constants>*//*</constants>*/
+    /*<methods>*//*</methods>*/
+
+    private static final /*<name>*/UIGuidedAccessAccessibilityFeature/*</name>*/[] values = _values(/*<name>*/UIGuidedAccessAccessibilityFeature/*</name>*/.class);
+
+    public /*<name>*/UIGuidedAccessAccessibilityFeature/*</name>*/(long value) { super(value); }
+    private /*<name>*/UIGuidedAccessAccessibilityFeature/*</name>*/(long value, long mask) { super(value, mask); }
+    protected /*<name>*/UIGuidedAccessAccessibilityFeature/*</name>*/ wrap(long value, long mask) {
+        return new /*<name>*/UIGuidedAccessAccessibilityFeature/*</name>*/(value, mask);
+    }
+    protected /*<name>*/UIGuidedAccessAccessibilityFeature/*</name>*/[] _values() {
+        return values;
+    }
+    public static /*<name>*/UIGuidedAccessAccessibilityFeature/*</name>*/[] values() {
+        return values.clone();
+    }
+}
