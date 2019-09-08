486315fac7059988b7ee3b28032d228d8e924621
dkimitsa <demyan.kimitsa@gmail.com>
2019-09-08 15:00:21 +0300

    ios13 bindings: CoreAudio processed

--- compiler/cocoatouch/src/main/java/org/robovm/apple/coreaudio/AudioFormat.java
+++ compiler/cocoatouch/src/main/java/org/robovm/apple/coreaudio/AudioFormat.java
@@ -66,6 +66,7 @@ public enum /*<name>*/AudioFormat/*</name>*/ implements ValuedEnum {
     MPEG4AAC_ELD_V2(1633772391L),
     MPEG4AAC_HE_V2(1633772400L),
     MPEG4AAC_Spatial(1633772403L),
+    MPEGD_USAC(1970495843L),
     AMR(1935764850L),
     AMR_WB(1935767394L),
     Audible(1096107074L),
