13368f72ef977364b0bc27fc98200864a39c1d25
Florian Otto <florian.otto@ecsec.de>
2019-10-10 15:40:56 +0200

    Move clean option to profile and expand build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
+
 set -e
-mvn clean install
+#if build environment changes (e.g. xcode path) this might be needed to clear cmake-cache in /compiler/vm/target
+if [ "$1" = '--fullclean' ];
+then
+    mvn clean install -Pclean-all
+else
+    mvn clean install 
+fi
 mvn -f plugins/idea/pom.xml clean install
 mvn -f plugins/eclipse/pom.xml clean install
 ./plugins/gradle/gradlew -b plugins/gradle/build.gradle clean assemble install
