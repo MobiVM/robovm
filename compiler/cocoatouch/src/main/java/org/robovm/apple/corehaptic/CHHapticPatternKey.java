686be6656eb4304ee30b3584077e546a8edab388
dkimitsa <demyan.kimitsa@gmail.com>
2020-08-20 12:47:39 +0300

    * ios14: CoreHaptics processed  (and keys class fixed)

--- compiler/cocoatouch/src/main/java/org/robovm/apple/corehaptic/CHHapticPatternKey.java
+++ compiler/cocoatouch/src/main/java/org/robovm/apple/corehaptic/CHHapticPatternKey.java
@@ -33,83 +33,203 @@ import org.robovm.apple.avfoundation.*;
 
 /*<javadoc>*/
 /*</javadoc>*/
-/*<annotations>*/@Library("CoreHaptics")/*</annotations>*/
+/*<annotations>*/@Library("CoreHaptics") @StronglyLinked/*</annotations>*/
+@Marshaler(/*<name>*/CHHapticPatternKey/*</name>*/.Marshaler.class)
 /*<visibility>*/public/*</visibility>*/ class /*<name>*/CHHapticPatternKey/*</name>*/ 
-    extends /*<extends>*/CocoaUtility/*</extends>*/ 
+    extends /*<extends>*/GlobalValueEnumeration<NSString>/*</extends>*/
     /*<implements>*//*</implements>*/ {
 
-    /*<ptr>*/
-    /*</ptr>*/
-    /*<bind>*/static { Bro.bind(CHHapticPatternKey.class); }/*</bind>*/
-    /*<constants>*//*</constants>*/
-    /*<constructors>*//*</constructors>*/
-    /*<properties>*//*</properties>*/
-    /*<members>*//*</members>*/
-    /*<methods>*/
+    static { Bro.bind(/*<name>*/CHHapticPatternKey/*</name>*/.class); }
+
+    /*<marshalers>*/
+    public static class Marshaler {
+        @MarshalsPointer
+        public static CHHapticPatternKey toObject(Class<CHHapticPatternKey> cls, long handle, long flags) {
+            NSString o = (NSString) NSObject.Marshaler.toObject(NSString.class, handle, flags);
+            if (o == null) {
+                return null;
+            }
+            return CHHapticPatternKey.valueOf(o);
+        }
+        @MarshalsPointer
+        public static long toNative(CHHapticPatternKey o, long flags) {
+            if (o == null) {
+                return 0L;
+            }
+            return NSObject.Marshaler.toNative(o.value(), flags);
+        }
+    }
+    public static class AsListMarshaler {
+        @SuppressWarnings("unchecked")
+        @MarshalsPointer
+        public static List<CHHapticPatternKey> toObject(Class<? extends NSObject> cls, long handle, long flags) {
+            NSArray<NSString> o = (NSArray<NSString>) NSObject.Marshaler.toObject(NSArray.class, handle, flags);
+            if (o == null) {
+                return null;
+            }
+            List<CHHapticPatternKey> list = new ArrayList<>();
+            for (int i = 0; i < o.size(); i++) {
+                list.add(CHHapticPatternKey.valueOf(o.get(i)));
+            }
+            return list;
+        }
+        @MarshalsPointer
+        public static long toNative(List<CHHapticPatternKey> l, long flags) {
+            if (l == null) {
+                return 0L;
+            }
+            NSArray<NSString> array = new NSMutableArray<>();
+            for (CHHapticPatternKey o : l) {
+                array.add(o.value());
+            }
+            return NSObject.Marshaler.toNative(array, flags);
+        }
+    }
+    /*</marshalers>*/
+
+    /*<constants>*/
     /**
      * @since Available in iOS 13.0 and later.
      */
-    @GlobalValue(symbol="CHHapticPatternKeyVersion", optional=true)
-    public static native NSString Version();
+    public static final CHHapticPatternKey Version = new CHHapticPatternKey("Version");
     /**
      * @since Available in iOS 13.0 and later.
      */
-    @GlobalValue(symbol="CHHapticPatternKeyPattern", optional=true)
-    public static native NSString Pattern();
+    public static final CHHapticPatternKey Pattern = new CHHapticPatternKey("Pattern");
     /**
      * @since Available in iOS 13.0 and later.
      */
-    @GlobalValue(symbol="CHHapticPatternKeyEvent", optional=true)
-    public static native NSString Event();
+    public static final CHHapticPatternKey Event = new CHHapticPatternKey("Event");
     /**
      * @since Available in iOS 13.0 and later.
      */
-    @GlobalValue(symbol="CHHapticPatternKeyEventType", optional=true)
-    public static native NSString EventType();
+    public static final CHHapticPatternKey EventType = new CHHapticPatternKey("EventType");
     /**
      * @since Available in iOS 13.0 and later.
      */
-    @GlobalValue(symbol="CHHapticPatternKeyTime", optional=true)
-    public static native NSString Time();
+    public static final CHHapticPatternKey Time = new CHHapticPatternKey("Time");
     /**
      * @since Available in iOS 13.0 and later.
      */
-    @GlobalValue(symbol="CHHapticPatternKeyEventDuration", optional=true)
-    public static native NSString EventDuration();
+    public static final CHHapticPatternKey EventDuration = new CHHapticPatternKey("EventDuration");
     /**
      * @since Available in iOS 13.0 and later.
      */
-    @GlobalValue(symbol="CHHapticPatternKeyEventWaveformPath", optional=true)
-    public static native NSString EventWaveformPath();
+    public static final CHHapticPatternKey EventWaveformPath = new CHHapticPatternKey("EventWaveformPath");
     /**
      * @since Available in iOS 13.0 and later.
      */
-    @GlobalValue(symbol="CHHapticPatternKeyEventParameters", optional=true)
-    public static native NSString EventParameters();
+    public static final CHHapticPatternKey EventParameters = new CHHapticPatternKey("EventParameters");
     /**
      * @since Available in iOS 13.0 and later.
      */
-    @GlobalValue(symbol="CHHapticPatternKeyParameter", optional=true)
-    public static native NSString Parameter();
+    public static final CHHapticPatternKey Parameter = new CHHapticPatternKey("Parameter");
     /**
      * @since Available in iOS 13.0 and later.
      */
-    @GlobalValue(symbol="CHHapticPatternKeyParameterID", optional=true)
-    public static native NSString ParameterID();
+    public static final CHHapticPatternKey ParameterID = new CHHapticPatternKey("ParameterID");
     /**
      * @since Available in iOS 13.0 and later.
      */
-    @GlobalValue(symbol="CHHapticPatternKeyParameterValue", optional=true)
-    public static native NSString ParameterValue();
+    public static final CHHapticPatternKey ParameterValue = new CHHapticPatternKey("ParameterValue");
     /**
      * @since Available in iOS 13.0 and later.
      */
-    @GlobalValue(symbol="CHHapticPatternKeyParameterCurve", optional=true)
-    public static native NSString ParameterCurve();
+    public static final CHHapticPatternKey ParameterCurve = new CHHapticPatternKey("ParameterCurve");
     /**
      * @since Available in iOS 13.0 and later.
      */
-    @GlobalValue(symbol="CHHapticPatternKeyParameterCurveControlPoints", optional=true)
-    public static native NSString ParameterCurveControlPoints();
-    /*</methods>*/
+    public static final CHHapticPatternKey ParameterCurveControlPoints = new CHHapticPatternKey("ParameterCurveControlPoints");
+    /*</constants>*/
+    
+    private static /*<name>*/CHHapticPatternKey/*</name>*/[] values = new /*<name>*/CHHapticPatternKey/*</name>*/[] {/*<value_list>*/Version, Pattern, Event, EventType, Time, EventDuration, EventWaveformPath, EventParameters, Parameter, ParameterID, ParameterValue, ParameterCurve, ParameterCurveControlPoints/*</value_list>*/};
+    
+    /*<name>*/CHHapticPatternKey/*</name>*/ (String getterName) {
+        super(Values.class, getterName);
+    }
+    
+    public static /*<name>*/CHHapticPatternKey/*</name>*/ valueOf(/*<type>*/NSString/*</type>*/ value) {
+        for (/*<name>*/CHHapticPatternKey/*</name>*/ v : values) {
+            if (v.value().equals(value)) {
+                return v;
+            }
+        }
+        throw new IllegalArgumentException("No constant with value " + value + " found in " 
+            + /*<name>*/CHHapticPatternKey/*</name>*/.class.getName());
+    }
+    
+    /*<methods>*//*</methods>*/
+    
+    /*<annotations>*/@Library("CoreHaptics") @StronglyLinked/*</annotations>*/
+    public static class Values {
+    	static { Bro.bind(Values.class); }
+
+        /*<values>*/
+        /**
+         * @since Available in iOS 13.0 and later.
+         */
+        @GlobalValue(symbol="CHHapticPatternKeyVersion", optional=true)
+        public static native NSString Version();
+        /**
+         * @since Available in iOS 13.0 and later.
+         */
+        @GlobalValue(symbol="CHHapticPatternKeyPattern", optional=true)
+        public static native NSString Pattern();
+        /**
+         * @since Available in iOS 13.0 and later.
+         */
+        @GlobalValue(symbol="CHHapticPatternKeyEvent", optional=true)
+        public static native NSString Event();
+        /**
+         * @since Available in iOS 13.0 and later.
+         */
+        @GlobalValue(symbol="CHHapticPatternKeyEventType", optional=true)
+        public static native NSString EventType();
+        /**
+         * @since Available in iOS 13.0 and later.
+         */
+        @GlobalValue(symbol="CHHapticPatternKeyTime", optional=true)
+        public static native NSString Time();
+        /**
+         * @since Available in iOS 13.0 and later.
+         */
+        @GlobalValue(symbol="CHHapticPatternKeyEventDuration", optional=true)
+        public static native NSString EventDuration();
+        /**
+         * @since Available in iOS 13.0 and later.
+         */
+        @GlobalValue(symbol="CHHapticPatternKeyEventWaveformPath", optional=true)
+        public static native NSString EventWaveformPath();
+        /**
+         * @since Available in iOS 13.0 and later.
+         */
+        @GlobalValue(symbol="CHHapticPatternKeyEventParameters", optional=true)
+        public static native NSString EventParameters();
+        /**
+         * @since Available in iOS 13.0 and later.
+         */
+        @GlobalValue(symbol="CHHapticPatternKeyParameter", optional=true)
+        public static native NSString Parameter();
+        /**
+         * @since Available in iOS 13.0 and later.
+         */
+        @GlobalValue(symbol="CHHapticPatternKeyParameterID", optional=true)
+        public static native NSString ParameterID();
+        /**
+         * @since Available in iOS 13.0 and later.
+         */
+        @GlobalValue(symbol="CHHapticPatternKeyParameterValue", optional=true)
+        public static native NSString ParameterValue();
+        /**
+         * @since Available in iOS 13.0 and later.
+         */
+        @GlobalValue(symbol="CHHapticPatternKeyParameterCurve", optional=true)
+        public static native NSString ParameterCurve();
+        /**
+         * @since Available in iOS 13.0 and later.
+         */
+        @GlobalValue(symbol="CHHapticPatternKeyParameterCurveControlPoints", optional=true)
+        public static native NSString ParameterCurveControlPoints();
+        /*</values>*/
+    }
 }
