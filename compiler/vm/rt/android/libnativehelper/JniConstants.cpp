1ed7c9c819a00dfe52557c364672b5bd5297fa1e
dkimitsa <demyan.kimitsa@gmail.com>
2020-09-30 17:17:48 +0300

    * Libcore10: RT natives adaptation: JniConstants were present in three folders: luni, ojluni and libnativehelper. Also there was conflict in function names. This is solved by moving all constants to single file to libnativehelper

--- compiler/vm/rt/android/libnativehelper/JniConstants.cpp
+++ compiler/vm/rt/android/libnativehelper/JniConstants.cpp
@@ -21,7 +21,6 @@
 
 #include <atomic>
 #include <mutex>
-#include <string>
 
 #include "nativehelper/ScopedLocalRef.h"
 
@@ -46,6 +45,43 @@ jclass g_nio_access_class = nullptr;       // java.nio.Access
 jclass g_nio_buffer_class = nullptr;       // java.nio.Buffer
 jclass g_reference_class = nullptr;        // java.lang.ref.Reference
 jclass g_string_class = nullptr;           // java.lang.String
+
+// RoboVM note: classes from luni folder
+jclass booleanClass;
+jclass charsetICUClass;
+jclass doubleClass;
+jclass errnoExceptionClass;
+jclass gaiExceptionClass;
+jclass inet6AddressClass;
+jclass inet6AddressHolderClass;
+jclass inetAddressClass;
+jclass inetAddressHolderClass;
+jclass inetSocketAddressClass;
+jclass inetSocketAddressHolderClass;
+jclass integerClass;
+jclass localeDataClass;
+jclass longClass;
+jclass netlinkSocketAddressClass;
+jclass packetSocketAddressClass;
+jclass patternSyntaxExceptionClass;
+jclass structAddrinfoClass;
+jclass structFlockClass;
+jclass structGroupReqClass;
+jclass structIfaddrsClass;
+jclass structLingerClass;
+jclass structPasswdClass;
+jclass structPollfdClass;
+jclass structStatClass;
+jclass structStatVfsClass;
+jclass structTimespecClass;
+jclass structTimevalClass;
+jclass structUcredClass;
+jclass structUtsnameClass;
+jclass unixSocketAddressClass;
+
+// RoboVM note: classes from ojluni folder
+jclass g_socket_tagger_class;
+
 
 // Cached field and method ids.
 //
@@ -106,7 +142,107 @@ jmethodID FindStaticMethod(JNIEnv* env, jclass klass, const char* name, const ch
     return result;
 }
 
+
+void EnsureClassReferencesInitialized(JNIEnv* env) {
+    // Fast check if class references are initialized.
+    if (g_class_refs_initialized.load(std::memory_order_acquire)) {
+        return;
+    }
+
+    // Slower check with initialization if necessary.
+    std::lock_guard<std::mutex> guard(g_class_refs_mutex);
+    if (g_class_refs_initialized.load(std::memory_order_relaxed)) {
+        return;
+    }
+
+    // Class constants should be initialized only once because they global
+    // references. Field ids and Method ids can be initialized later since they
+    // are not references and races only have trivial performance
+    // consequences.
+    g_file_descriptor_class = FindClass(env, "java/io/FileDescriptor");
+    g_nio_access_class = FindClass(env, "java/nio/NIOAccess");
+    g_nio_buffer_class = FindClass(env, "java/nio/Buffer");
+    g_reference_class = FindClass(env, "java/lang/ref/Reference");
+    g_string_class = FindClass(env, "java/lang/String");
+
+    // RoboVM note: classes from luni
+    booleanClass = FindClass(env, "java/lang/Boolean");
+    charsetICUClass = FindClass(env, "java/nio/charset/CharsetICU");
+    doubleClass = FindClass(env, "java/lang/Double");
+    errnoExceptionClass = FindClass(env, "android/system/ErrnoException");
+    gaiExceptionClass = FindClass(env, "android/system/GaiException");
+    inet6AddressClass = FindClass(env, "java/net/Inet6Address");
+    inet6AddressHolderClass = FindClass(env, "java/net/Inet6Address$Inet6AddressHolder");
+    inetAddressClass = FindClass(env, "java/net/InetAddress");
+    inetAddressHolderClass = FindClass(env, "java/net/InetAddress$InetAddressHolder");
+    inetSocketAddressClass = FindClass(env, "java/net/InetSocketAddress");
+    inetSocketAddressHolderClass = FindClass(env, "java/net/InetSocketAddress$InetSocketAddressHolder");
+    integerClass = FindClass(env, "java/lang/Integer");
+    localeDataClass = FindClass(env, "libcore/icu/LocaleData");
+    longClass = FindClass(env, "java/lang/Long");
+    netlinkSocketAddressClass = FindClass(env, "android/system/NetlinkSocketAddress");
+    packetSocketAddressClass = FindClass(env, "android/system/PacketSocketAddress");
+    patternSyntaxExceptionClass = FindClass(env, "java/util/regex/PatternSyntaxException");
+    structAddrinfoClass = FindClass(env, "android/system/StructAddrinfo");
+    structFlockClass = FindClass(env, "android/system/StructFlock");
+    structGroupReqClass = FindClass(env, "android/system/StructGroupReq");
+    structIfaddrsClass = FindClass(env, "android/system/StructIfaddrs");
+    structLingerClass = FindClass(env, "android/system/StructLinger");
+    structPasswdClass = FindClass(env, "android/system/StructPasswd");
+    structPollfdClass = FindClass(env, "android/system/StructPollfd");
+    structStatClass = FindClass(env, "android/system/StructStat");
+    structStatVfsClass = FindClass(env, "android/system/StructStatVfs");
+    structTimevalClass = FindClass(env, "android/system/StructTimeval");
+    structTimespecClass = FindClass(env, "android/system/StructTimespec");
+    structUcredClass = FindClass(env, "android/system/StructUcred");
+    structUtsnameClass = FindClass(env, "android/system/StructUtsname");
+    unixSocketAddressClass = FindClass(env, "android/system/UnixSocketAddress");
+
+    // RoboVM note: classes from ojluni
+    g_socket_tagger_class = FindClass(env, "dalvik/system/SocketTagger");
+
+    g_class_refs_initialized.store(true, std::memory_order_release);
+}
+
+void Uninitialize() {
+    // This method is called when a new runtime instance is created. There is no
+    // notification of a runtime instance being destroyed in the JNI interface
+    // so we piggyback on creation. Since only one runtime is supported at a
+    // time, we know the constants are invalid when JNI_CreateJavaVM() is
+    // called.
+    //
+    // Clean shutdown would require calling DeleteGlobalRef() for each of the
+    // class references.
+    std::lock_guard<std::mutex> guard(g_class_refs_mutex);
+    g_file_descriptor_class = nullptr;
+    g_file_descriptor_descriptor_field = nullptr;
+    g_file_descriptor_owner_id_field = nullptr;
+    g_file_descriptor_init_method = nullptr;
+    g_nio_access_class = nullptr;
+    g_nio_access_get_base_array_method = nullptr;
+    g_nio_access_get_base_array_offset_method = nullptr;
+    g_nio_buffer_class = nullptr;
+    g_nio_buffer_address_field = nullptr;
+    g_nio_buffer_element_size_shift_field = nullptr;
+    g_nio_buffer_limit_field = nullptr;
+    g_nio_buffer_position_field = nullptr;
+    g_nio_buffer_array_method = nullptr;
+    g_nio_buffer_array_offset_method = nullptr;
+    g_reference_class = nullptr;
+    g_reference_get_method = nullptr;
+    g_string_class = nullptr;
+    g_class_refs_initialized.store(false, std::memory_order_release);
+}
+
 }  // namespace
+
+void JniConstants::Initialize(JNIEnv* env) {
+    EnsureClassReferencesInitialized(env);
+}
+
+void JniConstants::Invalidate() {
+    Uninitialize();
+}
 
 jclass JniConstants::GetFileDescriptorClass(JNIEnv* env) {
     EnsureClassReferencesInitialized(env);
@@ -232,56 +368,165 @@ jmethodID JniConstants::GetReferenceGetMethod(JNIEnv* env) {
     return g_reference_get_method;
 }
 
-void JniConstants::EnsureClassReferencesInitialized(JNIEnv* env) {
-    // Fast check if class references are initialized.
-    if (g_class_refs_initialized.load(std::memory_order_acquire)) {
-        return;
-    }
-
-    // Slower check with initialization if necessary.
-    std::lock_guard<std::mutex> guard(g_class_refs_mutex);
-    if (g_class_refs_initialized.load(std::memory_order_relaxed)) {
-        return;
-    }
-
-    // Class constants should be initialized only once because they global
-    // references. Field ids and Method ids can be initialized later since they
-    // are not references and races only have trivial performance
-    // consequences.
-    g_file_descriptor_class = FindClass(env, "java/io/FileDescriptor");
-    g_nio_access_class = FindClass(env, "java/nio/NIOAccess");
-    g_nio_buffer_class = FindClass(env, "java/nio/Buffer");
-    g_reference_class = FindClass(env, "java/lang/ref/Reference");
-    g_string_class = FindClass(env, "java/lang/String");
-    g_class_refs_initialized.store(true, std::memory_order_release);
+// methods from luni
+jclass JniConstants::GetBooleanClass(JNIEnv* env) {
+    EnsureClassReferencesInitialized(env);
+    return booleanClass;
 }
 
-void JniConstants::Uninitialize() {
-    // This method is called when a new runtime instance is created. There is no
-    // notification of a runtime instance being destroyed in the JNI interface
-    // so we piggyback on creation. Since only one runtime is supported at a
-    // time, we know the constants are invalid when JNI_CreateJavaVM() is
-    // called.
-    //
-    // Clean shutdown would require calling DeleteGlobalRef() for each of the
-    // class references.
-    std::lock_guard<std::mutex> guard(g_class_refs_mutex);
-    g_file_descriptor_class = nullptr;
-    g_file_descriptor_descriptor_field = nullptr;
-    g_file_descriptor_owner_id_field = nullptr;
-    g_file_descriptor_init_method = nullptr;
-    g_nio_access_class = nullptr;
-    g_nio_access_get_base_array_method = nullptr;
-    g_nio_access_get_base_array_offset_method = nullptr;
-    g_nio_buffer_class = nullptr;
-    g_nio_buffer_address_field = nullptr;
-    g_nio_buffer_element_size_shift_field = nullptr;
-    g_nio_buffer_limit_field = nullptr;
-    g_nio_buffer_position_field = nullptr;
-    g_nio_buffer_array_method = nullptr;
-    g_nio_buffer_array_offset_method = nullptr;
-    g_reference_class = nullptr;
-    g_reference_get_method = nullptr;
-    g_string_class = nullptr;
-    g_class_refs_initialized.store(false, std::memory_order_release);
+jclass JniConstants::GetCharsetICUClass(JNIEnv* env) {
+    EnsureClassReferencesInitialized(env);
+    return charsetICUClass;
+}
+
+jclass JniConstants::GetDoubleClass(JNIEnv* env) {
+    EnsureClassReferencesInitialized(env);
+    return doubleClass;
+}
+
+jclass JniConstants::GetErrnoExceptionClass(JNIEnv* env) {
+    EnsureClassReferencesInitialized(env);
+    return errnoExceptionClass;
+}
+
+jclass JniConstants::GetGaiExceptionClass(JNIEnv* env) {
+    EnsureClassReferencesInitialized(env);
+    return gaiExceptionClass;
+}
+
+jclass JniConstants::GetInet6AddressClass(JNIEnv* env) {
+    EnsureClassReferencesInitialized(env);
+    return inet6AddressClass;
+}
+
+jclass JniConstants::GetInet6AddressHolderClass(JNIEnv* env) {
+    EnsureClassReferencesInitialized(env);
+    return inet6AddressHolderClass;
+}
+
+jclass JniConstants::GetInetAddressClass(JNIEnv* env) {
+    EnsureClassReferencesInitialized(env);
+    return inetAddressClass;
+}
+
+jclass JniConstants::GetInetAddressHolderClass(JNIEnv* env) {
+    EnsureClassReferencesInitialized(env);
+    return inetAddressHolderClass;
+}
+
+jclass JniConstants::GetInetSocketAddressClass(JNIEnv* env) {
+    EnsureClassReferencesInitialized(env);
+    return inetSocketAddressClass;
+}
+
+jclass JniConstants::GetInetSocketAddressHolderClass(JNIEnv* env) {
+    EnsureClassReferencesInitialized(env);
+    return inetSocketAddressHolderClass;
+}
+
+jclass JniConstants::GetIntegerClass(JNIEnv* env) {
+    EnsureClassReferencesInitialized(env);
+    return integerClass;
+}
+
+jclass JniConstants::GetLocaleDataClass(JNIEnv* env) {
+    EnsureClassReferencesInitialized(env);
+    return localeDataClass;
+}
+
+jclass JniConstants::GetLongClass(JNIEnv* env) {
+    EnsureClassReferencesInitialized(env);
+    return longClass;
+}
+
+jclass JniConstants::GetNetlinkSocketAddressClass(JNIEnv* env) {
+    EnsureClassReferencesInitialized(env);
+    return netlinkSocketAddressClass;
+}
+
+jclass JniConstants::GetPacketSocketAddressClass(JNIEnv* env) {
+    EnsureClassReferencesInitialized(env);
+    return packetSocketAddressClass;
+}
+
+jclass JniConstants::GetPatternSyntaxExceptionClass(JNIEnv* env) {
+    EnsureClassReferencesInitialized(env);
+    return patternSyntaxExceptionClass;
+}
+
+
+jclass JniConstants::GetStructAddrinfoClass(JNIEnv* env) {
+    EnsureClassReferencesInitialized(env);
+    return structAddrinfoClass;
+}
+
+jclass JniConstants::GetStructFlockClass(JNIEnv* env) {
+    EnsureClassReferencesInitialized(env);
+    return structFlockClass;
+}
+
+jclass JniConstants::GetStructGroupReqClass(JNIEnv* env) {
+    EnsureClassReferencesInitialized(env);
+    return structGroupReqClass;
+}
+
+jclass JniConstants::GetStructIfaddrsClass(JNIEnv* env) {
+    EnsureClassReferencesInitialized(env);
+    return structIfaddrsClass;
+}
+
+jclass JniConstants::GetStructLingerClass(JNIEnv* env) {
+    EnsureClassReferencesInitialized(env);
+    return structLingerClass;
+}
+
+jclass JniConstants::GetStructPasswdClass(JNIEnv* env) {
+    EnsureClassReferencesInitialized(env);
+    return structPasswdClass;
+}
+
+jclass JniConstants::GetStructPollfdClass(JNIEnv* env) {
+    EnsureClassReferencesInitialized(env);
+    return structPollfdClass;
+}
+
+jclass JniConstants::GetStructStatClass(JNIEnv* env) {
+    EnsureClassReferencesInitialized(env);
+    return structStatClass;
+}
+
+jclass JniConstants::GetStructStatVfsClass(JNIEnv* env) {
+    EnsureClassReferencesInitialized(env);
+    return structStatVfsClass;
+}
+
+jclass JniConstants::GetStructTimespecClass(JNIEnv* env) {
+    EnsureClassReferencesInitialized(env);
+    return structTimespecClass;
+}
+
+jclass JniConstants::GetStructTimevalClass(JNIEnv* env) {
+    EnsureClassReferencesInitialized(env);
+    return structTimevalClass;
+}
+
+jclass JniConstants::GetStructUcredClass(JNIEnv* env) {
+    EnsureClassReferencesInitialized(env);
+    return structUcredClass;
+}
+
+jclass JniConstants::GetStructUtsnameClass(JNIEnv* env) {
+    EnsureClassReferencesInitialized(env);
+    return structUtsnameClass;
+}
+
+jclass JniConstants::GetUnixSocketAddressClass(JNIEnv* env) {
+    EnsureClassReferencesInitialized(env);
+    return unixSocketAddressClass;
+}
+
+// RoboVM note: methods from ojluni
+jclass JniConstants::GetSocketTaggerClass(JNIEnv* env) {
+    EnsureClassReferencesInitialized(env);
+    return g_socket_tagger_class;
 }
