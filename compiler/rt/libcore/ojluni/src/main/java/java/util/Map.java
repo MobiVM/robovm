dbb1923eb9de1c95e3f222ad4141aabb89bbaee7
dkimitsa <demyan.kimitsa@gmail.com>
2020-09-30 11:38:14 +0300

    * Libcore10: RT adaptation: removed Serializable interfaces on lambdas. As it requires support for SerializedLambda which is missing (due not full support of dynamic invoke in RoboVM). Otherwise it breaks compilation with error message: > error: cannot find symbol > symbol: method getImplMethodKind() > location: interface SerializedLambda

--- compiler/rt/libcore/ojluni/src/main/java/java/util/Map.java
+++ compiler/rt/libcore/ojluni/src/main/java/java/util/Map.java
@@ -468,7 +468,8 @@ public interface Map<K, V> {
          * @since 1.8
          */
         public static <K extends Comparable<? super K>, V> Comparator<Map.Entry<K, V>> comparingByKey() {
-            return (Comparator<Map.Entry<K, V>> & Serializable)
+            // RoboVM note: not extending Serializable due to lack of proper lambda support
+            return (Comparator<Map.Entry<K, V>> /* RoboVM & Serializable */)
                 (c1, c2) -> c1.getKey().compareTo(c2.getKey());
         }
 
@@ -485,7 +486,8 @@ public interface Map<K, V> {
          * @since 1.8
          */
         public static <K, V extends Comparable<? super V>> Comparator<Map.Entry<K, V>> comparingByValue() {
-            return (Comparator<Map.Entry<K, V>> & Serializable)
+            // RoboVM note: not extending Serializable due to lack of proper lambda support
+            return (Comparator<Map.Entry<K, V>> /* RoboVM & Serializable */)
                 (c1, c2) -> c1.getValue().compareTo(c2.getValue());
         }
 
@@ -504,7 +506,8 @@ public interface Map<K, V> {
          */
         public static <K, V> Comparator<Map.Entry<K, V>> comparingByKey(Comparator<? super K> cmp) {
             Objects.requireNonNull(cmp);
-            return (Comparator<Map.Entry<K, V>> & Serializable)
+            // RoboVM note: not extending Serializable due to lack of proper lambda support
+            return (Comparator<Map.Entry<K, V>> /* RoboVM & Serializable*/)
                 (c1, c2) -> cmp.compare(c1.getKey(), c2.getKey());
         }
 
@@ -523,7 +526,8 @@ public interface Map<K, V> {
          */
         public static <K, V> Comparator<Map.Entry<K, V>> comparingByValue(Comparator<? super V> cmp) {
             Objects.requireNonNull(cmp);
-            return (Comparator<Map.Entry<K, V>> & Serializable)
+            // RoboVM note: not extending Serializable due to lack of proper lambda support
+            return (Comparator<Map.Entry<K, V>> /* RoboVM: & Serializable */)
                 (c1, c2) -> cmp.compare(c1.getValue(), c2.getValue());
         }
     }
