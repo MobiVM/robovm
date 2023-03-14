b5f7ada39f22cd16dbac450f78d1c35937545084
dkimitsa <demyan.kimitsa@gmail.com>
2023-03-14 17:45:18 +0200

    * maven fix: 3.9 -- connection reset, reverting to wagon transport that uses retries

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e
-mvn -Dmaven.wagon.httpconnectionManager.ttlSeconds=30 -Dhttp.keepAlive=false -Dmaven.wagon.http.retryHandler.count=3 clean install
+mvn clean install
 ./plugins/idea/gradlew -b plugins/idea/build.gradle clean buildPlugin
-mvn -Dmaven.wagon.httpconnectionManager.ttlSeconds=30 -Dhttp.keepAlive=false -Dmaven.wagon.http.retryHandler.count=3 -f plugins/eclipse/pom.xml clean install
+mvn -f plugins/eclipse/pom.xml clean install
 ./plugins/gradle/gradlew -b plugins/gradle/build.gradle clean assemble publishToMavenLocal
