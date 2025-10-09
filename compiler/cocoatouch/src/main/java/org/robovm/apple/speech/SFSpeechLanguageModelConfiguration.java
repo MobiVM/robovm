c0218eedc6d83d7b608399ec9f76fb9635babfde
dkimitsa <demyan.kimitsa@gmail.com>
2025-10-09 23:40:42 +0300

    * iOS26 bindings: Speech updated

--- compiler/cocoatouch/src/main/java/org/robovm/apple/speech/SFSpeechLanguageModelConfiguration.java
+++ compiler/cocoatouch/src/main/java/org/robovm/apple/speech/SFSpeechLanguageModelConfiguration.java
@@ -40,7 +40,7 @@ import org.robovm.apple.coremedia.*;
 /*<annotations>*/@Library("Speech") @NativeClass/*</annotations>*/
 /*<visibility>*/public/*</visibility>*/ class /*<name>*/SFSpeechLanguageModelConfiguration/*</name>*/ 
     extends /*<extends>*/NSObject/*</extends>*/ 
-    /*<implements>*//*</implements>*/ {
+    /*<implements>*/implements NSSecureCoding/*</implements>*/ {
 
     /*<ptr>*/public static class SFSpeechLanguageModelConfigurationPtr extends Ptr<SFSpeechLanguageModelConfiguration, SFSpeechLanguageModelConfigurationPtr> {}/*</ptr>*/
     /*<bind>*/static { ObjCRuntime.bind(SFSpeechLanguageModelConfiguration.class); }/*</bind>*/
@@ -53,12 +53,26 @@ import org.robovm.apple.coremedia.*;
     public SFSpeechLanguageModelConfiguration(NSURL languageModel) { super((SkipInit) null); initObject(init(languageModel)); }
     @Method(selector = "initWithLanguageModel:vocabulary:")
     public SFSpeechLanguageModelConfiguration(NSURL languageModel, NSURL vocabulary) { super((SkipInit) null); initObject(init(languageModel, vocabulary)); }
+    /**
+     * @since Available in iOS 26.0 and later.
+     */
+    @Method(selector = "initWithLanguageModel:vocabulary:weight:")
+    public SFSpeechLanguageModelConfiguration(NSURL languageModel, NSURL vocabulary, NSNumber weight) { super((SkipInit) null); initObject(init(languageModel, vocabulary, weight)); }
+    @Method(selector = "initWithCoder:")
+    public SFSpeechLanguageModelConfiguration(NSCoder coder) { super((SkipInit) null); initObject(init(coder)); }
     /*</constructors>*/
     /*<properties>*/
     @Property(selector = "languageModel")
     public native NSURL getLanguageModel();
     @Property(selector = "vocabulary")
     public native NSURL getVocabulary();
+    /**
+     * @since Available in iOS 26.0 and later.
+     */
+    @Property(selector = "weight")
+    public native NSNumber getWeight();
+    @Property(selector = "supportsSecureCoding")
+    public static native boolean supportsSecureCoding();
     /*</properties>*/
     /*<members>*//*</members>*/
     /*<methods>*/
@@ -66,5 +80,14 @@ import org.robovm.apple.coremedia.*;
     protected native @Pointer long init(NSURL languageModel);
     @Method(selector = "initWithLanguageModel:vocabulary:")
     protected native @Pointer long init(NSURL languageModel, NSURL vocabulary);
+    /**
+     * @since Available in iOS 26.0 and later.
+     */
+    @Method(selector = "initWithLanguageModel:vocabulary:weight:")
+    protected native @Pointer long init(NSURL languageModel, NSURL vocabulary, NSNumber weight);
+    @Method(selector = "encodeWithCoder:")
+    public native void encode(NSCoder coder);
+    @Method(selector = "initWithCoder:")
+    protected native @Pointer long init(NSCoder coder);
     /*</methods>*/
 }
