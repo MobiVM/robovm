7214b4d34e1eb4a2f87f64bff8e6a3467b34483d
dkimitsa <demyan.kimitsa@gmail.com>
2025-10-11 20:24:47 +0300

    * iOS26 bindings: SafetyKit -- new framework

---
+++ compiler/cocoatouch/src/main/java/org/robovm/apple/safetykit/SACrashDetectionManager.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright (C) 2013-2015 RoboVM AB
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.robovm.apple.safetykit;
+
+/*<imports>*/
+import java.io.*;
+import java.nio.*;
+import java.util.*;
+import org.robovm.objc.*;
+import org.robovm.objc.annotation.*;
+import org.robovm.objc.block.*;
+import org.robovm.rt.*;
+import org.robovm.rt.annotation.*;
+import org.robovm.rt.bro.*;
+import org.robovm.rt.bro.annotation.*;
+import org.robovm.rt.bro.ptr.*;
+import org.robovm.apple.foundation.*;
+import org.robovm.apple.corelocation.*;
+/*</imports>*/
+
+/*<javadoc>*/
+/**
+ * @since Available in iOS 16.0 and later.
+ */
+/*</javadoc>*/
+/*<annotations>*/@Library("SafetyKit") @NativeClass/*</annotations>*/
+/*<visibility>*/public/*</visibility>*/ class /*<name>*/SACrashDetectionManager/*</name>*/ 
+    extends /*<extends>*/NSObject/*</extends>*/ 
+    /*<implements>*//*</implements>*/ {
+
+    /*<ptr>*/public static class SACrashDetectionManagerPtr extends Ptr<SACrashDetectionManager, SACrashDetectionManagerPtr> {}/*</ptr>*/
+    /*<bind>*/static { ObjCRuntime.bind(SACrashDetectionManager.class); }/*</bind>*/
+    /*<constants>*//*</constants>*/
+    /*<constructors>*/
+    public SACrashDetectionManager() {}
+    protected SACrashDetectionManager(Handle h, long handle) { super(h, handle); }
+    protected SACrashDetectionManager(SkipInit skipInit) { super(skipInit); }
+    /*</constructors>*/
+    /*<properties>*/
+    @Property(selector = "isAvailable")
+    public static native boolean isAvailable();
+    @Property(selector = "authorizationStatus")
+    public native SAAuthorizationStatus getAuthorizationStatus();
+    @Property(selector = "delegate")
+    public native SACrashDetectionDelegate getDelegate();
+    @Property(selector = "setDelegate:", strongRef = true)
+    public native void setDelegate(SACrashDetectionDelegate v);
+    /*</properties>*/
+    /*<members>*//*</members>*/
+    /*<methods>*/
+    @Method(selector = "requestAuthorizationWithCompletionHandler:")
+    public native void requestAuthorization(@Block VoidBlock2<SAAuthorizationStatus, NSError> handler);
+    /*</methods>*/
+}
