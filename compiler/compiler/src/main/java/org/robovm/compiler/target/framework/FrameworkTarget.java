f2a9878a1ebf456c91929b08e7548006401c0b32
dkimitsa <demyan.kimitsa@gmail.com>
2025-11-21 17:34:39 +0200

    * fixed: Framework target, variant filtering on libs/frameworks wasn't applied when dynamic frameworks were copied

--- compiler/compiler/src/main/java/org/robovm/compiler/target/framework/FrameworkTarget.java
+++ compiler/compiler/src/main/java/org/robovm/compiler/target/framework/FrameworkTarget.java
@@ -311,7 +311,13 @@ public class FrameworkTarget extends AbstractTarget {
 					executable = new File(new File(config.getTmpDir(), identifier), getExecutable());
 				}
 				config.getLogger().info("Installing " + identifier + "framework slice: %s", frameworkDir);
-				installFramework(frameworkDir, dsymDir, executable, image);
+
+                // create slice config and slice target for install operation as
+                // there should be different variant filtering while copying dependencies
+                Config sliceConfig = config.builder().archs(archesInBinary).build();
+                FrameworkTarget sliceTarget = new FrameworkTarget(XC_TYPE);
+                sliceTarget.init(sliceConfig);
+				sliceTarget.installFramework(frameworkDir, dsymDir, executable, image);
 
 				XCFrameworkPlist.Library library = new XCFrameworkPlist.Library(identifier,
 						image + ".framework", config.getOs(),
