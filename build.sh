14a4b6475a899e9dae368ba42f3c4545cc4b2b4a
dkimitsa <demyan.kimitsa@gmail.com>
2026-06-28 13:21:09 +0300

    * fix: eclipse build JAXP00010003: The length of entity "[xml]" is "100001" that exceeds the "100000" constraint are introduced by JDK-8343004 and JDK-8343006, but seems like P2 repo has long xmls. temporally disabling this check for Eclipse build

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 set -e
 mvn -T 10 clean install
 ./plugins/idea/gradlew -p plugins/idea/ clean buildPlugin
-mvn -T 10 -f plugins/eclipse/pom.xml clean install
+MAVEN_OPTS="-Djdk.xml.maxGeneralEntitySizeLimit=0 -Djdk.xml.totalEntitySizeLimit=0" mvn -T 10 -f plugins/eclipse/pom.xml clean install
 ./plugins/gradle/gradlew -b plugins/gradle/build.gradle clean assemble validatePlugins publishToMavenLocal
